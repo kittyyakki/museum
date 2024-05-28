44b9f0fd9b6e8bcfa1f84344fbd4cc4d00c1ce8f
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-05-28 17:05:02 +0900

    QnaVO추가

---
+++ src/main/java/com/team4/museum/vo/QnaVO.java
@@ -0,0 +1,60 @@
+package com.team4.museum.vo;
+
+import java.sql.Date;
+
+public class QnaVO {
+	
+	private Integer qseq;
+	private String title;
+	private String content;
+	private Date writedate;
+	private String email;
+	private String pwd;
+	private String phone;
+	
+	public Integer getQseq() {
+		return qseq;
+	}
+	public void setQseq(Integer qseq) {
+		this.qseq = qseq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(Date writedate) {
+		this.writedate = writedate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
