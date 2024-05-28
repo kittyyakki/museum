01fce3b759236ba27aaafe8d1526ce211f1e3f24
JJS <rpffkwns@naver.com>
2024-05-28 16:25:07 +0900

    Update MemberGalleryVO.java

--- src/main/java/com/team4/museum/vo/MemberGalleryVO.java
+++ src/main/java/com/team4/museum/vo/MemberGalleryVO.java
@@ -2,4 +2,72 @@ package com.team4.museum.vo;
 
 public class MemberGalleryVO {
 	
+	private int mseq;
+	private String author;
+	private String title;
+	private int aseq;
+	private String content;
+	
+	
+	
+	public int getMseq() {
+		return mseq;
+	}
+	public void setMseq(int mseq) {
+		this.mseq = mseq;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getAseq() {
+		return aseq;
+	}
+	public void setAseq(int aseq) {
+		this.aseq = aseq;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getSavefilename() {
+		return savefilename;
+	}
+	public void setSavefilename(String savefilename) {
+		this.savefilename = savefilename;
+	}
+	public int getLikecount() {
+		return likecount;
+	}
+	public void setLikecount(int likecount) {
+		this.likecount = likecount;
+	}
+	private int readcount;
+	private String image;
+	private String savefilename;
+	private int likecount;
+	
 }
+	
