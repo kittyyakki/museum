e05a16d8b183d73b0d2a8413124dfbcb0f6d3c75
yyh7986 <yyh7986@gmail.com>
2024-05-28 16:12:36 +0900

    MemberVO추가

---
+++ src/main/java/com/team4/museum/vo/MemberVO.java
@@ -0,0 +1,11 @@
+package com.team4.museum.vo;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private String id;
+	private String name;
+	private String  pwd;
+	private String  email;
+	private Date indate;
+}
