610ca7350c15c635bd4b59c22f6aead7fa8d63b8
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-05-29 18:22:17 +0900

    member.sql insert 추가

---
+++ src/main/webapp/database/insert_member.sql
@@ -0,0 +1,6 @@
+--회원 입력 --
+insert into member(id,name,pwd,email,phone)
+values('one','김나리','1111','abc@naver.com','010-3333-4444');
+--관리자 입력--
+insert into member(id,name,pwd,email,phone,adminyn)
+values('admin','관리자','1111','def@naver.com','010-5555-6666','Y');
