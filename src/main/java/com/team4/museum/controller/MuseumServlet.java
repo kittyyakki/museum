cc4f77a92098fb79afdccb286411b15a86451348
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-05-29 16:42:27 +0900

    Yyh (#1)
    * artwork 이미지 폴더로 이동
    
    * 카테고리 enum 추가
    
    * 헤더, 푸터 css 추가
    
    * header.jsp 추가
    
    * footer.jsp 추가
    
    * 기본 액션 추가
    
    * 로고 사진 등록
    
    * index.jsp 추가
    
    * main.jsp 수정
    
    * MuseumServlet 추가

---
+++ src/main/java/com/team4/museum/controller/MuseumServlet.java
@@ -0,0 +1,44 @@
+package com.team4.museum.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import com.team4.museum.controller.action.Action;
+
+public class MuseumServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public MuseumServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		String command = request.getParameter("command");
+		if (command == null) {
+			System.out.println("Command not found");
+			return;
+		}
+
+		Action ac = ActionFactory.getInstance().getAction(command);
+		if (ac == null) {
+			System.out.println("Action not found : " + command);
+			return;
+		}
+
+		ac.execute(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
