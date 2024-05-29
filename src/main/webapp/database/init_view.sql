d6496fe4964a2995ce267ad08d14cc28cdb29d95
PresentKim <debe3721@gmail.com>
2024-05-29 10:55:35 +0900

    DB 뷰 SQL 파일 추가

---
+++ src/main/webapp/database/init_view.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE VIEW member_gallery_view AS
+SELECT mseq,
+       author as author_id,
+       name   as author_name,
+       title,
+       writedate,
+       content,
+       readcount,
+       image,
+       savefilename,
+       likecount
+FROM member_gallery
+         INNER JOIN member ON member_gallery.author = member.id
+ORDER BY member_gallery.writedate;
+
+
+CREATE OR REPLACE VIEW review_view AS
+SELECT rseq,
+       author as author_id,
+       name   as author_name,
+       title,
+       writedate,
+       content
+FROM review
+         INNER JOIN member ON review.author = member.id
+ORDER BY review.writedate;
+
+
+CREATE OR REPLACE VIEW favorite_view AS
+SELECT artwork.*,
+       writedate
+FROM favorite_artwork
+         INNER JOIN artwork ON favorite_artwork.id = artwork.aseq
+ORDER BY favorite_artwork.writedate;
