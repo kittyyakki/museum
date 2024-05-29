fc38b98137e2dbb020fb4167aa2dfde5bf0effab
PresentKim <debe3721@gmail.com>
2024-05-29 12:50:17 +0900

    NoticeDAO 클래스 추가

---
+++ src/main/java/com/team4/museum/dao/NoticeDAO.java
@@ -0,0 +1,90 @@
+package com.team4.museum.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.team4.museum.vo.NoticeVO;
+import static com.team4.museum.util.Db.*;
+
+final public class NoticeDAO {
+
+	private NoticeDAO() {
+	}
+
+	private static final NoticeDAO instance = new NoticeDAO();
+
+	public static NoticeDAO getInstance() {
+		return instance;
+	}
+
+	public List<NoticeVO> selectNoticeList() {
+		return executeSelect(
+				"SELECT * FROM notice",
+				NoticeDAO::extractNoticeVO);
+	}
+
+	public List<NoticeVO> selectNoticeList(int limit, int offset) {
+		return executeSelect(
+				"SELECT * FROM notice LIMIT ? OFFSET ?",
+				pstmt -> {
+					pstmt.setInt(1, limit);
+					pstmt.setInt(2, offset);
+				},
+				NoticeDAO::extractNoticeVO);
+	}
+
+	public NoticeVO selectNotice(int nseq) {
+		return executeSelectOne(
+				"SELECT * FROM notice WHERE nseq = ?",
+				pstmt -> pstmt.setInt(1, nseq), NoticeDAO::extractNoticeVO);
+	}
+
+	public int insertNotice(NoticeVO notice) {
+		return executeUpdate(
+				"INSERT INTO notice (title, author, content, category) VALUES (?, ?, ?, ?)",
+				pstmt -> {
+					pstmt.setString(1, notice.getTitle());
+					pstmt.setString(2, notice.getAuthor());
+					pstmt.setString(3, notice.getContent());
+					pstmt.setInt(4, notice.getCategory());
+				});
+	}
+
+	public int updateNotice(NoticeVO notice) {
+		return executeUpdate(
+				"UPDATE notice SET title = ?, author = ?, content = ?, category = ? WHERE nseq = ?",
+				pstmt -> {
+					pstmt.setString(1, notice.getTitle());
+					pstmt.setString(2, notice.getAuthor());
+					pstmt.setString(3, notice.getContent());
+					pstmt.setInt(4, notice.getCategory());
+					pstmt.setInt(5, notice.getNseq());
+				});
+	}
+
+	public int deleteNotice(int nseq) {
+		return executeUpdate(
+				"DELETE FROM notice WHERE nseq = ?",
+				pstmt -> pstmt.setInt(1, nseq));
+	}
+
+	public int getNoticeCount() {
+		return executeSelectOne(
+				"SELECT COUNT(*) FROM notice",
+				rs -> rs.getInt(1));
+	}
+
+	private static NoticeVO extractNoticeVO(ResultSet rs) throws SQLException {
+		NoticeVO notice = new NoticeVO();
+		notice.setNseq(rs.getInt("nseq"));
+		notice.setTitle(rs.getString("title"));
+		notice.setAuthor(rs.getString("author"));
+		notice.setWritedate(rs.getDate("writedate"));
+		notice.setContent(rs.getString("content"));
+		notice.setReadcount(rs.getInt("readcount"));
+		notice.setCategory(rs.getInt("category"));
+		return notice;
+	}
+
+}
