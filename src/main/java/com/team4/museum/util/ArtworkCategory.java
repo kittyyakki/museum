443c77177142713130963b882339ba30c422978a
yyh7986 <yyh7986@gmail.com>
2024-05-29 16:34:24 +0900

    카테고리 enum 추가

---
+++ src/main/java/com/team4/museum/util/ArtworkCategory.java
@@ -0,0 +1,5 @@
+package com.team4.museum.util;
+
+enum ArtworkCategory {
+	전체, 회화, 드로잉, 판화, 조각ㆍ설치, 사진, 공예, 디자인, 서예;
+}
---
+++ src/main/java/com/team4/museum/util/NoticeCategory.java
@@ -0,0 +1,5 @@
+package com.team4.museum.util;
+
+enum NoticeCategory{
+	공지사항, 이벤트, 매거진, 신문;
+}
