6020e712ff3a43bb8366759714550363cf480d0b
yyh7986 <yyh7986@gmail.com>
2024-05-29 12:49:26 +0900

    MemberGalleryVO 수정

--- src/main/java/com/team4/museum/vo/MemberGalleryVO.java
+++ src/main/java/com/team4/museum/vo/MemberGalleryVO.java
@@ -1,85 +1,76 @@
 package com.team4.museum.vo;
 
+import java.sql.Date;
+
 public class MemberGalleryVO {
 
 	private int mseq;
-	private String author;
+	private String authorId;
+	private String authorName;
 	private String title;
-	private int aseq;
+	private Date writedate;
 	private String content;
 	private int readcount;
 	private String image;
 	private String savefilename;
 	private int likecount;
-
 	public int getMseq() {
 		return mseq;
 	}
-
 	public void setMseq(int mseq) {
 		this.mseq = mseq;
 	}
-
-	public String getAuthor() {
-		return author;
+	public String getAuthorId() {
+		return authorId;
 	}
-
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setAuthorId(String authorId) {
+		this.authorId = authorId;
 	}
-
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-	public int getAseq() {
-		return aseq;
+	public Date getWritedate() {
+		return writedate;
 	}
-
-	public void setAseq(int aseq) {
-		this.aseq = aseq;
+	public void setWritedate(Date writedate) {
+		this.writedate = writedate;
 	}
-
 	public String getContent() {
 		return content;
 	}
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
 	public int getReadcount() {
 		return readcount;
 	}
-
 	public void setReadcount(int readcount) {
 		this.readcount = readcount;
 	}
-
 	public String getImage() {
 		return image;
 	}
-
 	public void setImage(String image) {
 		this.image = image;
 	}
-
 	public String getSavefilename() {
 		return savefilename;
 	}
-
 	public void setSavefilename(String savefilename) {
 		this.savefilename = savefilename;
 	}
-
 	public int getLikecount() {
 		return likecount;
 	}
-
 	public void setLikecount(int likecount) {
 		this.likecount = likecount;
 	}
