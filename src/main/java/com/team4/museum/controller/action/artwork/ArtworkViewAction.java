1a1e7948e07eb2ab2127aad5abd8e55ce370f480
yyh7986 <yyh7986@gmail.com>
2024-05-30 18:20:38 +0900

    feat: artworkView 액션 추가

---
+++ src/main/java/com/team4/museum/controller/action/artwork/ArtworkViewAction.java
@@ -0,0 +1,26 @@
+package com.team4.museum.controller.action.artwork;
+
+import java.io.IOException;
+
+import com.team4.museum.controller.action.Action;
+import com.team4.museum.dao.ArtworkDao;
+import com.team4.museum.vo.ArtworkVO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ArtworkViewAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int aseq = Integer.parseInt(request.getParameter("aseq"));
+		
+		ArtworkDao adao = ArtworkDao.getInstance();
+		ArtworkVO avo = adao.selectArtworkOne(aseq);
+		
+		request.setAttribute("artwork", avo);
+		request.getRequestDispatcher("artwork/artworkView.jsp").forward(request, response);
+	}
+
+}
