921e11855eac872ae875bb6f09d66fab188ff72f
JJS <rpffkwns@naver.com>
2024-05-30 14:34:35 +0900

    ActionFactory 내용 추가

--- src/main/java/com/team4/museum/controller/ActionFactory.java
+++ src/main/java/com/team4/museum/controller/ActionFactory.java
@@ -1,12 +1,15 @@
 package com.team4.museum.controller;
 
 import com.team4.museum.controller.action.Action;
+import com.team4.museum.controller.action.IdcheckFormAction;
 import com.team4.museum.controller.action.IndexAction;
+import com.team4.museum.controller.action.JoinAction;
+import com.team4.museum.controller.action.JoinFormAction;
 
 public class ActionFactory {
 
 	private ActionFactory() {
-	}
+	}	
 
 	private static final ActionFactory instance = new ActionFactory();
 
@@ -18,7 +21,12 @@ public class ActionFactory {
 		return switch (command) {
 
 		case "index" -> new IndexAction();
+		case "join" -> new JoinAction();
+		case "joinForm" -> new JoinFormAction();
+		case "idcheckForm" -> new IdcheckFormAction();
 		default -> null;
+		
+		
 
 		};
 	}
