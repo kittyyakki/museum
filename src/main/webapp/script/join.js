a85c4fed40e18a203a6df932045470749251e362
JJS <rpffkwns@naver.com>
2024-05-30 18:19:49 +0900

    feat: EditMemberAction 추가

--- src/main/webapp/script/join.js
+++ src/main/webapp/script/join.js
@@ -40,5 +40,26 @@ function idok( id ){
 	opener.joinForm.reid.value =id;
 	self.close();
 }	
+
+function go_editMember(){
+	if( document.joinForm.pwd.value == "") {
+	    alert("비밀번호를 입력해 주세요.");	    
+	    document.joinForm.pwd.focus();
+	} else if( document.joinForm.pwd.value != document.joinForm.pwdCheck.value) {
+	    alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");	    
+	    document.joinForm.pwd.focus();
+	} else if( document.joinForm.name.value == "") {
+	    alert("이름을 입력해 주세요.");	    
+	    document.joinForm.name.focus();
+	} else if( document.joinForm.phone.value == "") {
+	    alert("전화번호를 입력해 주세요.");	   
+	    document.joinForm.phone.focus();
+	}else if( document.joinForm.email.value == "") {
+	    alert("이메일을 입력해 주세요.");	   
+	    document.joinForm.email.focus();
+	} else{
+	    document.joinForm.submit();
+	}
 	
+}	
 
