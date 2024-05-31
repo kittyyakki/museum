23b78fe36307e8f1a07ff454b6eda66ae0d1eff4
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-05-31 17:37:44 +0900

    feat:(NoticeAction.java)

--- src/main/java/com/team4/museum/controller/action/notice/NoticeAction.java
+++ src/main/java/com/team4/museum/controller/action/notice/NoticeAction.java
@@ -1,41 +1,29 @@
 package com.team4.museum.controller.action.notice;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+
 
 import com.team4.museum.controller.action.Action;
 import com.team4.museum.dao.NoticeDAO;
-import com.team4.museum.util.Paging;
 import com.team4.museum.vo.NoticeVO;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 public class NoticeAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 게시판의 게시물을 모두 조회해서  request 에 담고 noticeList.jsp  로  포워딩합니다
 		NoticeDAO ndao = NoticeDAO.getInstance();
-		Paging paging = new Paging();
 
-		int count = ndao.getNoticeAllCount();
-		paging.setTotalCount(count);
-
-		List<NoticeVO> list = ndao.selectNoticeList(paging);
-
-		for (NoticeVO nvo : list) {
-			count = ndao.getReplyCount(nvo.getNseq());
-			nvo.setReadcount(count);
-
-		}
+		List<NoticeVO> list = ndao.selectNoticeList(1, 10);
 		request.setAttribute("noticeList", list);
-		request.setAttribute("paging", paging);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("noticeList.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("notice/noticeList.jsp");
 		rd.forward(request, response);
 
 	}
