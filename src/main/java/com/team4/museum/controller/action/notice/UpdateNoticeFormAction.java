db6b755be5ff2e0543e4de9c1cedb315e3ef5535
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-05-31 15:06:40 +0900

    feat:(UpdateNoticeFormAction)

---
+++ src/main/java/com/team4/museum/controller/action/notice/UpdateNoticeFormAction.java
@@ -0,0 +1,29 @@
+package com.team4.museum.controller.action.notice;
+
+import java.io.IOException;
+
+
+import com.team4.museum.controller.action.Action;
+import com.team4.museum.dao.NoticeDAO;
+import com.team4.museum.vo.NoticeVO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UpdateNoticeFormAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int nseq = Integer.parseInt( request.getParameter("nseq") );
+		NoticeDAO ndao = NoticeDAO.getInstance();
+		// 게시물 상세 조회
+		NoticeVO nvo = ndao.selectNotice(nseq);
+		
+		request.setAttribute("notice", nvo);
+		request.getRequestDispatcher("notice/updateNoticeForm.jsp").forward(request, response);
+
+	}
+
+}
