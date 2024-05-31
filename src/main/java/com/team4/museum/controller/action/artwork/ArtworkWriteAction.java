8a8f01a899423185bfd17e5d72f19c132e5b6929
yyh7986 <yyh7986@gmail.com>
2024-05-31 17:37:15 +0900

    feat: 예술품 등록 폼에서 카테고리 선택을 위한 파라미터값 추가

--- src/main/java/com/team4/museum/controller/action/artwork/ArtworkWriteAction.java
+++ src/main/java/com/team4/museum/controller/action/artwork/ArtworkWriteAction.java
@@ -3,6 +3,7 @@ package com.team4.museum.controller.action.artwork;
 import java.io.IOException;
 
 import com.team4.museum.controller.action.Action;
+import com.team4.museum.util.ArtworkCategory;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ public class ArtworkWriteAction implements Action{
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("category",  ArtworkCategory.values());
 		request.getRequestDispatcher("artwork/artworkWriteForm.jsp").forward(request, response);
 	}
 
