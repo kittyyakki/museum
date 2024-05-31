18c082b97064cd6de5c661475752e87da1b7b6bd
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-05-31 15:07:14 +0900

    feat:(UpdateNoticeAction.java)

---
+++ src/main/java/com/team4/museum/controller/action/notice/UpdateNoticeAction.java
@@ -0,0 +1,76 @@
+package com.team4.museum.controller.action.notice;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+
+import com.team4.museum.controller.action.Action;
+import com.team4.museum.dao.MemberDao;
+import com.team4.museum.dao.NoticeDAO;
+import com.team4.museum.vo.MemberVO;
+import com.team4.museum.vo.NoticeVO;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+
+public class UpdateNoticeAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+		NoticeDAO ndao = NoticeDAO.getInstance();
+		NoticeVO nvo = new NoticeVO();
+		
+		MemberDao mdao = MemberDao.getInstance();
+		MemberVO mvo = new MemberVO();
+		
+		nvo.setNseq( Integer.parseInt( request.getParameter("nseq") ) );
+		mvo.setId( request.getParameter("id") ) ;
+		mvo.setPwd( request.getParameter("pwd") );
+		nvo.setTitle( request.getParameter("title") );
+		nvo.setContent( request.getParameter("content") );
+		
+		
+		HttpSession session = request.getSession();
+		ServletContext context = session.getServletContext();
+		String uploadFilePath = context.getRealPath("images");
+		File uploadDir = new File(uploadFilePath);
+		if(!uploadDir.exists()) uploadDir.mkdir();
+		
+		String fileName="";
+		String saveFilename = "";
+		for( Part p: request.getParts() ) {
+			//System.out.println(p.getHeader("content-disposition") ); 
+			fileName = "";
+			for (String content : p.getHeader("content-disposition").split(";")) {
+			      if(content.trim().startsWith("filename")) { 
+			    	  fileName = content.substring(content.indexOf("=")+2, content.length()-1);
+			    	  System.out.println("filename : " + fileName);
+			    	  if(!fileName.equals("") ) {
+							Calendar today = Calendar.getInstance();
+							long dt = today.getTimeInMillis();
+							String fn1 = fileName.substring(0, fileName.indexOf(".")  );
+							String fn2 = fileName.substring( fileName.indexOf(".") );
+							saveFilename =  fn1 + dt + fn2;
+							p.write(uploadFilePath + File.separator + saveFilename); // 파일 저장
+							nvo.setImage(fileName);
+							nvo.setSavefilename(saveFilename);
+						}else {
+							nvo.setImage( request.getParameter("oldimage"));
+							nvo.setSavefilename(request.getParameter("oldsavefilename"));
+						}
+			      }
+		    }	
+		}
+		ndao.updateNotice( nvo );
+		
+		response.sendRedirect("museum.do?command=noticeViewWithoutCnt&nseq=" + nvo.getNseq() );
+		
+	}
+	
+}
