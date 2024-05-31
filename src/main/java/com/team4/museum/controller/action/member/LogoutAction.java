5c0ae2f0d66a1610a6acf3612ba50adf330d02e4
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-05-31 11:00:56 +0900

    refactor: (LogoutAction.java)

--- src/main/java/com/team4/museum/controller/action/member/LogoutAction.java
+++ src/main/java/com/team4/museum/controller/action/member/LogoutAction.java
@@ -13,7 +13,7 @@ public class LogoutAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		session.removeAttribute("loginUser");
-		request.getRequestDispatcher("member/loginForm.jsp").forward(request, response);
+		request.getRequestDispatcher("main.jsp").forward(request, response);
 
 	}
 }
