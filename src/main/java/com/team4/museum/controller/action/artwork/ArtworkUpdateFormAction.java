5770ed73c9df38e0ef8bee0a904a6b67607b8aa5
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-06-02 21:19:55 +0900

    feat(ArtworkUpdateFormAction): 예술품 수정기능 액션추가

---
+++ src/main/java/com/team4/museum/controller/action/artwork/ArtworkUpdateFormAction.java
@@ -0,0 +1,64 @@
+package com.team4.museum.controller.action.artwork;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+
+import com.team4.museum.controller.action.Action;
+import com.team4.museum.dao.ArtworkDao;
+import com.team4.museum.vo.ArtworkVO;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+
+public class ArtworkUpdateFormAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArtworkVO avo = new ArtworkVO();
+		int aseq = Integer.parseInt(request.getParameter("aseq"));
+		avo.setAseq(aseq);
+		avo.setArtist(request.getParameter("artist"));
+		avo.setName(request.getParameter("artname"));
+		avo.setYear(request.getParameter("year"));
+		avo.setMaterial(request.getParameter("material"));
+		avo.setSize(request.getParameter("size"));
+		avo.setCategory(request.getParameter("category"));
+		avo.setDisplayyn(request.getParameter("displayYn"));
+		avo.setContent(request.getParameter("content"));
+		
+		HttpSession session = request.getSession();
+		ServletContext context = session.getServletContext();
+		String uploadFilePath = context.getRealPath("images/artwork");
+		
+		File uploadDir = new File(uploadFilePath);
+		if(!uploadDir.exists()) uploadDir.mkdir();
+		
+		String fileName="";
+		for( Part p: request.getParts() ) {
+			fileName = "";
+			for (String content : p.getHeader("content-disposition").split(";")) {
+			      if(content.trim().startsWith("filename")) 
+			    	  fileName = content.substring(content.indexOf("=")+2, content.length()-1);
+			}
+			String saveFilename = "";
+			if(!fileName.equals("") ) {
+				Calendar today = Calendar.getInstance();
+				long dt = today.getTimeInMillis();
+				String fn1 = fileName.substring(0, fileName.indexOf(".")  );
+				String fn2 = fileName.substring( fileName.indexOf(".") );
+				saveFilename =  fn1 + dt + fn2;
+				p.write(uploadFilePath + File.separator + saveFilename); // 파일 저장
+				avo.setImage(fileName);
+				avo.setSavefilename(saveFilename);
+			}
+		}
+		
+		ArtworkDao.getInstance().updateArtwork(avo);
+		request.getRequestDispatcher("museum.do?command=artworkView&aseq=" + aseq).forward(request, response);
+	}
+}
