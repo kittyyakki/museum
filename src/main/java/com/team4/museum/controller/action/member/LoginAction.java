7e652eb7ca5b1b19b624fe9dce74e47279c42591
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-06-02 21:48:07 +0900

    refactor(LoginAction): 세션값 returnUrl 기능제거

--- src/main/java/com/team4/museum/controller/action/member/LoginAction.java
+++ src/main/java/com/team4/museum/controller/action/member/LoginAction.java
@@ -18,19 +18,15 @@ public class LoginAction implements Action {
 		MemberVO mvo = MemberDao.getInstance().getMember(id);
 
 		HttpSession session = request.getSession();
-		String returnUrl = (String) session.getAttribute("returnUrl");
 		
 		String url = "member/loginForm.jsp";
 		if (mvo == null) {
 			request.setAttribute("message", "아이디가 없습니다");
 		} else if (!mvo.getPwd().equals(pwd)) {
 			request.setAttribute("message", "패스워드가 틀립니다");
-		}else if(returnUrl != null) {	
-			// artworkView.jsp 에서 관심작품으로 추가하기를 클릭했을때 로그아웃상태인 경우에 로그인후 해당 작품으로 돌아가기 위해 설정
-			url = returnUrl;
 		}else {
 			url = "museum.do?command=index";
-			request.getSession().setAttribute("loginUser", mvo);
+			session.setAttribute("loginUser", mvo);
 		}
 
 		request.getRequestDispatcher(url).forward(request, response);
