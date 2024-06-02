366b40d6c7efb3cb05de5e8c0f36423866f41aac
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-06-02 21:21:34 +0900

    refactor(ArtworkListAction): 이용자와 관리자 기능 분리

--- src/main/java/com/team4/museum/controller/action/artwork/ArtworkListAction.java
+++ src/main/java/com/team4/museum/controller/action/artwork/ArtworkListAction.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import com.team4.museum.util.ArtworkCategory;
 import com.team4.museum.vo.ArtworkVO;
+import com.team4.museum.vo.MemberVO;
 import com.team4.museum.controller.action.Action;
 import com.team4.museum.dao.ArtworkDao;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class ArtworkListAction implements Action {
 
@@ -19,6 +21,13 @@ public class ArtworkListAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
+		
+		HttpSession session = request.getSession();
+		
+		// artworkView 페이지에서 '목록으로'이동하기 위해 현재 조회중인 category를 세션에 저장
+		session.removeAttribute("category");
+		
+		MemberVO mvo = (MemberVO)session.getAttribute("loginUser");
 
 		ArtworkDao adao = ArtworkDao.getInstance();
 		String category = request.getParameter("category") == null ? ArtworkCategory.전체.name()
@@ -29,20 +38,35 @@ public class ArtworkListAction implements Action {
 
 		List<ArtworkVO> list = null;
 
-		// 메인페이지에서 "예술품" 메뉴를 선택했을때 category == 0 (분류 : 전체)
-		if (searchWord != null) { // 검색어로 조회
-			list = adao.selectSearchArtwork(searchWord);
-			titleState = "off";
-		} else if (category.equals(ArtworkCategory.전체.name())) // 전체목록 조회
-			list = adao.selectArtwork();
-		else { // 카테고리 조회
-			list = adao.selectCategoryArtwork(category);
+		// 관리자 계정으로 로그인한 경우
+		if(mvo != null && mvo.getAdminyn().equals("Y")) {
+			if (searchWord != null) { // 검색어로 조회
+				list = adao.searchArtworkAdmin(searchWord);
+				category = null;
+//				titleState = "off";
+			} else if (category.equals(ArtworkCategory.전체.name())) // 전체목록 조회
+				list = adao.selectArtworkAdmin();				
+			else { // 카테고리 조회
+				list = adao.selectCategoryArtworkAdmin(category);
+			}
+		// 관리자 계정이 아닌경우 displayyn=="N" 인 예술품이 표시되지 않아야함
+		}else {
+			if (searchWord != null) { // 검색어로 조회
+				list = adao.searchArtwork(searchWord);
+				category = null;
+//				titleState = "off";
+			} else if (category.equals(ArtworkCategory.전체.name())) // 전체목록 조회
+				list = adao.selectArtwork();				
+			else { // 카테고리 조회
+				list = adao.selectCategoryArtwork(category);
+			}
 		}
-
+		
 		request.setAttribute("searchWord", searchWord);
-		request.setAttribute("titleState", titleState);
+//		request.setAttribute("titleState", titleState);
 		// 카테고리 이름 전달
 		request.setAttribute("categoryName", category);
+		session.setAttribute("category", category);
 		// 카테고리로 조회한 예술품 리스트 전달
 		request.setAttribute("artworkList", list);
 		// 분류명 목록을 배열로 전달
