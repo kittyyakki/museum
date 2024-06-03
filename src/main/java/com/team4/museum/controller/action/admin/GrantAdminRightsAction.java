3f642342aabf5a78fb00ae87913030bbdfa65145
yyh7986 <yyh7986@gmail.com>
2024-06-03 18:04:53 +0900

    feat: 관리자 권한 부여 기능추가 진행중

---
+++ src/main/java/com/team4/museum/controller/action/admin/GrantAdminRightsAction.java
@@ -0,0 +1,18 @@
+package com.team4.museum.controller.action.admin;
+
+import java.io.IOException;
+
+import com.team4.museum.controller.action.Action;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class GrantAdminRightsAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
