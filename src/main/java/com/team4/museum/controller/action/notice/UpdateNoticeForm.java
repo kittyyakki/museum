746c031e88115804ac22c915accb9ac05370ece9
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-06-03 09:39:19 +0900

    feat:(UpdateNoticeForm.java)

--- src/main/java/com/team4/museum/controller/action/notice/UpdateNoticeForm.java
+++ src/main/java/com/team4/museum/controller/action/notice/UpdateNoticeForm.java
@@ -19,7 +19,7 @@ public class UpdateNoticeForm implements Action{
 		int nseq = Integer.parseInt( request.getParameter("nseq") );
 		NoticeDAO ndao = NoticeDAO.getInstance();
 		// 게시물 상세 조회
-		NoticeVO nvo = ndao.selectNotice(nseq);
+		NoticeVO nvo = ndao.getNotice(nseq);
 		
 		request.setAttribute("notice", nvo);
 	}
