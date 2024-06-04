bf68405ecab9a322145abd7c7d3a707f2d8976bc
PresentKim <debe3721@gmail.com>
2024-06-05 02:01:04 +0900

    refactor(ArtworkWriteFormAction): MultipartFileInfo를 이용해 파일을 저장하고 사용

--- src/main/java/com/team4/museum/controller/action/artwork/ArtworkWriteFormAction.java
+++ src/main/java/com/team4/museum/controller/action/artwork/ArtworkWriteFormAction.java
@@ -1,21 +1,17 @@
 package com.team4.museum.controller.action.artwork;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Calendar;
 
 import com.team4.museum.controller.action.Action;
 import com.team4.museum.dao.ArtworkDao;
+import com.team4.museum.util.MultipartFileInfo;
 import com.team4.museum.vo.ArtworkVO;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import jakarta.servlet.http.Part;
 
-public class ArtworkWriteFormAction implements Action{
+public class ArtworkWriteFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,34 +25,11 @@ public class ArtworkWriteFormAction implements Action{
 		avo.setSize(request.getParameter("size"));
 		avo.setDisplayyn(request.getParameter("displayYn"));
 		avo.setContent(request.getParameter("content"));
-		
-		HttpSession session = request.getSession();
-		ServletContext context = session.getServletContext();
-		String uploadFilePath = context.getRealPath("images/artwork");
-		
-		File uploadDir = new File(uploadFilePath);
-		if(!uploadDir.exists()) uploadDir.mkdir();
-		
-		String fileName="";
-		for( Part p: request.getParts() ) {
-			fileName = "";
-			for (String content : p.getHeader("content-disposition").split(";")) {
-			      if(content.trim().startsWith("filename")) 
-			    	  fileName = content.substring(content.indexOf("=")+2, content.length()-1);
-			}
-			String saveFilename = "";
-			if(!fileName.equals("") ) {
-				Calendar today = Calendar.getInstance();
-				long dt = today.getTimeInMillis();
-				String fn1 = fileName.substring(0, fileName.indexOf(".")  );
-				String fn2 = fileName.substring( fileName.indexOf(".") );
-				saveFilename =  fn1 + dt + fn2;
-				p.write(uploadFilePath + File.separator + saveFilename); // 파일 저장
-				avo.setImage(fileName);
-				avo.setSavefilename(saveFilename);
-			}
-		}
-		
+
+		MultipartFileInfo info = MultipartFileInfo.saveFileFrom(request, "images/artwork");
+		avo.setImage(info.getFileName());
+		avo.setSavefilename(info.getSaveFileName());
+
 		adao.insertArtwork(avo);
 		request.getRequestDispatcher("museum.do?command=artworkList").forward(request, response);
 	}
