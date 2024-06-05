852177ae49f4c1d516f8bda8bab96bad5ac4addd
PresentKim <debe3721@gmail.com>
2024-06-05 17:59:51 +0900

    refactor(JoinAction): JoinAction를 AJAX 방식으로 변경

--- src/main/java/com/team4/museum/controller/action/member/JoinFormAction.java
+++ src/main/java/com/team4/museum/controller/action/member/JoinFormAction.java
@@ -3,6 +3,7 @@ package com.team4.museum.controller.action.member;
 import java.io.IOException;
 
 import com.team4.museum.controller.action.Action;
+import com.team4.museum.util.UrlUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,9 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JoinFormAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setAttribute("returnUrl", UrlUtil.encode(request.getParameter("returnUrl")));
 		request.getRequestDispatcher("member/joinForm.jsp").forward(request, response);
-
 	}
-
 }
