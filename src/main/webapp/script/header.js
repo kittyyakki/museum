b4d5befb183cdb42e4746d31f0df8c708b2c25e5
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-06-07 00:10:28 +0900

    feat:(header.js)

--- src/main/webapp/script/header.js
+++ src/main/webapp/script/header.js
@@ -1,24 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const link1 = document.querySelector('.header_gnb > a[href="museum.do?command=artworkList"]');
-	const container = document.querySelector('.header_gnb_list_container');
+jQuery.noConflict();
+jQuery(document).ready(function($) {
+    
+    var run_delayed = {
+        playing: false,
+        
+        play: function() {
+            $('#logo_outline01').stop().animate({'fill-opacity':0},500);
+            $('#logo_outline02').stop().animate({'fill-opacity':0},500);
+            $('#logo_outline03').stop().animate({'fill-opacity':0},500);
+            $('#logo_outline04').stop().animate({'fill-opacity':0},500);
+            $('#logo_outline05').stop().animate({'fill-opacity':0},500);
+            $('#logo_outline06').stop().animate({'fill-opacity':0},500);
+            $('#logo_outline07').stop().animate({'fill-opacity':0},500);
+            
+            new Vivus("header_logo_v", {
+                type: "delayed",
+                start: "autostart",
+                delay: 250,
+                duration: 300,
+            }, function(){
+                $('#logo_outline01').stop().animate({'fill-opacity':1},500);
+                $('#logo_outline02').stop().animate({'fill-opacity':1},500);
+                $('#logo_outline03').stop().animate({'fill-opacity':1},500);
+                $('#logo_outline04').stop().animate({'fill-opacity':1},500);
+                $('#logo_outline05').stop().animate({'fill-opacity':1},500);
+                $('#logo_outline06').stop().animate({'fill-opacity':1},500);
+                $('#logo_outline07').stop().animate({'fill-opacity':1},500);
+            });
+            
+            this.playing = true;
+        },
+        
+        stop: function() {
+            // Additional logic if needed
+            this.playing = false;
+        }
+    };
+    
+    run_delayed.play();
 
-	if (link1 && container) {
-		link1.addEventListener('mouseover', () => {
-			container.style.display = 'block';
-		});
-
-		link1.addEventListener('mouseout', () => {
-			container.style.display = 'none';
-		});
-
-		container.addEventListener('mouseover', () => {
-			container.style.display = 'block';
-		});
-
-		container.addEventListener('mouseout', () => {
-			container.style.display = 'none';
-		});
-	} else {
-		console.error('Required elements not found');
-	}
+    $('#header_logo_v').click(function(e){
+        e.preventDefault();
+        if(!run_delayed.playing){
+            run_delayed.play();
+        }
+    });
 });
