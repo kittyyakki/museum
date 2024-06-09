75f03aa50a287ec5a1351bcc0da91103190718db
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-06-09 17:30:10 +0900

    style: 파일 이름 변경 AdminQnaDeleteAction -> AdminDeleteQnaAction

--- src/main/java/com/team4/museum/controller/action/admin/AdminDeleteQnaAction.java
+++ src/main/java/com/team4/museum/controller/action/admin/AdminDeleteQnaAction.java
@@ -9,14 +9,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AdminQnaDeleteAction implements Action {
+public class AdminDeleteQnaAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String[] memberIds = request.getParameter("memberIds").split(",");
+		String[] qseqList = request.getParameter("memberIds").split(",");
 
-		for (String id : memberIds) {
-			QnaDao.getInstance().deleteQna(Integer.parseInt(id));
+		for (String qseq : qseqList) {
+			QnaDao.getInstance().deleteQna(Integer.parseInt(qseq));
 		}
 
 		request.getRequestDispatcher("museum.do?command=adminQnaList").forward(request, response);
