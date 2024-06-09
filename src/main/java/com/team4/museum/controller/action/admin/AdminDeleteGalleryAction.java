5220506e43930bd8d03fb53d390bef50df2c66ef
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-06-09 17:36:48 +0900

    feat(AdminDeleteGalleryAction): 관리자페이지 갤러리 삭제기능 추가

---
+++ src/main/java/com/team4/museum/controller/action/admin/AdminDeleteGalleryAction.java
@@ -0,0 +1,25 @@
+package com.team4.museum.controller.action.admin;
+
+import java.io.IOException;
+
+import com.team4.museum.controller.action.Action;
+import com.team4.museum.dao.MemberGalleryDao;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AdminDeleteGalleryAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] mseqList = request.getParameter("memberIds").split(",");
+
+		for (String mseq : mseqList) {
+			MemberGalleryDao.getInstance().deleteMemberGallery(Integer.parseInt(mseq));
+		}
+
+		request.getRequestDispatcher("museum.do?command=adminMemberGalleryList").forward(request, response);
+	}
+
+}
