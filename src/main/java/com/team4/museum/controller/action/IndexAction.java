44b201cb1497551f4a5fee5d9d836d2ff6f3da18
YongHo Yang <163955303+yyh7986@users.noreply.github.com>
2024-06-10 23:21:04 +0900

    feat(IndexAction): 메인페이지 슬라이더 구현

--- src/main/java/com/team4/museum/controller/action/IndexAction.java
+++ src/main/java/com/team4/museum/controller/action/IndexAction.java
@@ -1,9 +1,13 @@
 package com.team4.museum.controller.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import com.team4.museum.dao.ArtworkDao;
 import com.team4.museum.dao.NoticeDao;
+import com.team4.museum.vo.ArtworkVO;
 import com.team4.museum.vo.NoticeVO;
 
 import jakarta.servlet.ServletException;
@@ -15,9 +19,16 @@ public class IndexAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		NoticeDao ndao = NoticeDao.getInstance();
-	      List<NoticeVO> noticeList = ndao.getRecentNotice();
-	      request.setAttribute("noticeList", noticeList);
+		ArtworkDao adao = ArtworkDao.getInstance();
+
+		for(int i = 1; i <= 4; i++) {
+			List<ArtworkVO> randomArtworkList = adao.getRandomList();
+			request.setAttribute("artworkList" + i, randomArtworkList);			
+		}
 		
+		List<NoticeVO> noticeList = ndao.getRecentNotice();
+		request.setAttribute("noticeList", noticeList);
+
 		request.getRequestDispatcher("/WEB-INF/views/main/main.jsp").forward(request, response);
 	}
 
