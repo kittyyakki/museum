d6b88b80e4a519a79c21ec0085516249923ddd78
PresentKim <debe3721@gmail.com>
2024-06-11 19:05:36 +0900

    feat(ContractAcion): 이미 로그인 되어있는 경우 메인 화면으로 이동

--- src/main/java/com/team4/museum/controller/action/member/ContractAcion.java
+++ src/main/java/com/team4/museum/controller/action/member/ContractAcion.java
@@ -13,6 +13,12 @@ public class ContractAcion implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 이미 로그인 되어있는 경우
+		if (LoginAjaxAction.isLoginedFrom(request)) {
+			response.sendRedirect("museum.do?command=index");
+			return;
+		}
+
 		// 파라미터의 'returnUrl'을 인코딩하여 속성으로 함께 전달
 		// JSP에서 action 경로 뒤에 붙여야 하기 때문에 인코딩이 필요함
 		request.setAttribute("returnUrl", UrlUtil.encode(request.getParameter("returnUrl")));
