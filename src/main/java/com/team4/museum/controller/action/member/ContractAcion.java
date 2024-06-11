1e1741f4226cda9ed45aa4cdb1574985d611a92e
PresentKim <debe3721@gmail.com>
2024-06-11 18:23:22 +0900

    fix(ContractAcion): 이전 페이지로 돌아갈 수 있도록 정보를 전달

--- src/main/java/com/team4/museum/controller/action/member/ContractAcion.java
+++ src/main/java/com/team4/museum/controller/action/member/ContractAcion.java
@@ -3,18 +3,20 @@ package com.team4.museum.controller.action.member;
 import java.io.IOException;
 
 import com.team4.museum.controller.action.Action;
+import com.team4.museum.util.UrlUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class ContractAcion implements Action{
-	
+public class ContractAcion implements Action {
 
-		@Override
-		public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
-			request.getRequestDispatcher("/WEB-INF/views/member/contract.jsp").forward(request, response);
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 파라미터의 'returnUrl'을 인코딩하여 속성으로 함께 전달
+		// JSP에서 action 경로 뒤에 붙여야 하기 때문에 인코딩이 필요함
+		request.setAttribute("returnUrl", UrlUtil.encode(request.getParameter("returnUrl")));
+		request.getRequestDispatcher("/WEB-INF/views/member/contract.jsp").forward(request, response);
+	}
 
-		}
 }
