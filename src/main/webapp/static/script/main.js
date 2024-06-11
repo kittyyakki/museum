81d04d3d5e1fd2829788a68a01aac7672806b8fb
PresentKim <debe3721@gmail.com>
2024-06-11 15:26:41 +0900

    fix(main): 옆으로 넘어가는 슬라이드 잘못된 작동 수정

--- src/main/webapp/static/script/main.js
+++ src/main/webapp/static/script/main.js
@@ -1,82 +1,64 @@
-$(function() {
-	var imgNum = 0;
-	var timer;
-	var state = false;
-	$('#main-center-remote div').click(
-		function() {
-			var idx = $(this).index();
-			var dist;
-			if (idx == 8) { // 자동 버튼
-				if (state == false) { // 자동동작 멈춤상태
-					state = true;
-					$('#main-center-remote div:eq(8)').html('||');
-					timer = window.setInterval(function() {
-						imgNum++;
-						if (imgNum > 7)
-							imgNum = 0;
-						dist = -100 * imgNum; // 너비 비율을 100%로 설정
-						$('#main-center-imgs').animate({
-							left: dist + '%'
-						}, 1000);
-						$('#main-center-remote div').removeClass('selected');
-						$('#main-center-remote div').eq(imgNum).addClass('selected');
-					}, 3500);
-				} else { // 자동동작 상태
-					state = false;
-					$('#main-center-remote div:eq(8)').html('▶');
-					window.clearInterval(timer);
-				}
-			} else {
-				imgNum = idx;
-				dist = -100 * imgNum; // 너비 비율을 100%로 설정
-				$('#main-center-imgs').animate({
-					left: dist + '%'
-				}, 1000);
-			}
-			$('#main-center-remote div').removeClass('selected');
-			$('#main-center-remote div').eq(imgNum).addClass('selected');
-		});
-});
+function swiperRun(swiper) {
+	// 이미지 컨테이너 요소와 이미지 요소 배열
+	var imageContainer = swiper.querySelectorAll('.main-swiper_image-container')[0];
+	var imageCount = imageContainer.children.length;
 
+	// 리모콘 이미지 선택 버튼 생성
+	var remoteContainer = swiper.querySelectorAll('.main-swiper_remote-container')[0];
+	for (var i = 0; i < imageCount; i++) {
+		remoteContainer.appendChild(document.createElement('div'));
+	}
 
-$(function() {
-	var imgNum = 0;
-	var timer;
-	var state = false;
-	$('#main-center-remote02 div').click(
-		function() {
-			var idx = $(this).index();
-			var dist;
-			if (idx == 4) { // 자동 버튼
-				if (state == false) { // 자동동작 멈춤상태
-					state = true;
-					$('#main-center-remote02 div:eq(4)').html('||');
-					timer = window.setInterval(function() {
-						imgNum++;
-						if (imgNum > 3)
-							imgNum = 0;
-						dist = -100 * imgNum; // 너비 비율을 100%로 설정
-						$('#main-center-imgs02').animate({
-							left: dist + '%'
-						}, 1000);
-						$('#main-center-remote02 div').removeClass('selected');
-						$('#main-center-remote02 div').eq(imgNum).addClass('selected');
-					}, 3500);
-				} else { // 자동동작 상태
-					state = false;
-					$('#main-center-remote02 div:eq(4)').html('▶');
-					window.clearInterval(timer);
-				}
-			} else {
-				imgNum = idx;
-				dist = -100 * imgNum; // 너비 비율을 100%로 설정
-				$('#main-center-imgs02').animate({
-					left: dist + '%'
-				}, 1000);
-			}
-			$('#main-center-remote02 div').removeClass('selected');
-			$('#main-center-remote02 div').eq(imgNum).addClass('selected');
-		});
-});
+	// 리모콘 재생/일시정지 버튼 생성
+	var remotePauseButton = document.createElement('div');
+	remotePauseButton.classList.add('pause-button');
+	remoteContainer.appendChild(remotePauseButton);
 
+	// 이미지 선택 함수
+	var imageIndex = 0;
+	var timer = -1;
+	function selectImage(newIndex) {
+		// 이미지 인덱스 계산 (0 ~ imageCount - 1)
+		imageIndex = newIndex % imageCount;
 
+		// 이미지 이동 처리
+		imageContainer.style.left = -imageIndex * 100 + '%';
+
+		// 버튼 선택 상태 변경
+		for (var i = 0; i < imageCount; i++) {
+			remoteContainer.children[i].classList.remove('selected');
+		}
+		remoteContainer.children[imageIndex].classList.add('selected');
+
+		// 이미 타이머가 동작 중이라면 중지하고 다시 시작
+		if (timer !== -1) {
+			togglePlay();
+			togglePlay();
+		}
+	}
+
+	// 재생 / 일시정지 함수
+	function togglePlay() {
+		if (timer !== -1) {
+			clearInterval(timer);
+			timer = -1;
+			remotePauseButton.innerText = '▶';
+		} else {
+			timer = setInterval(() => {
+				selectImage(imageIndex + 1);
+			}, 3000);
+			remotePauseButton.innerText = '||';
+		}
+	}
+
+	// 이벤트 핸들러 추가
+	for (var i = 0; i < imageCount; i++) {
+		const index = i;
+		remoteContainer.children[i].addEventListener('click', function() { selectImage(index); });
+	}
+	remotePauseButton.addEventListener('click', togglePlay);
+
+	// 초기 이미지 선택 및 타이머 시작
+	selectImage(0);
+	togglePlay();
+}
