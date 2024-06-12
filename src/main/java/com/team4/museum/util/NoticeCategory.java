b57fe95a7f5b01c9f76d2703e8f1a9813bcdcee9
PresentKim <debe3721@gmail.com>
2024-06-12 10:13:15 +0900

    feat(NoticeCategory): 작성 가능한 카테고리만 반환하는 `writableValues()` 메소드 추가
    - 공지사항, 이벤트

--- src/main/java/com/team4/museum/util/NoticeCategory.java
+++ src/main/java/com/team4/museum/util/NoticeCategory.java
@@ -1,5 +1,11 @@
 package com.team4.museum.util;
 
-public enum NoticeCategory{
+import java.util.List;
+
+public enum NoticeCategory {
 	전체, 공지사항, 이벤트, 매거진, 신문;
+
+	public static List<NoticeCategory> writableValues() {
+		return List.of(공지사항, 이벤트);
+	}
 }
